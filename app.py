d26d224cb6021249c7ef3ca59e7e52e4c2d12796
Rahulkumar010 <thatikondarahulkumar2000@gmail.com>
2024-02-24 01:17:49 +0530

    git update

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
-from markupsafe import  escape
+from markupsafe import escape
+import git
+
 
 app = Flask(__name__)
+
+@app.route("/git_update")
+def git_update():
+    repo = git.Repo('./sample-flask-app')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '',200
 
 @app.route("/")
 def home():
